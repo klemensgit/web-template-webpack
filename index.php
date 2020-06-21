bbcb8cced055abfca87000964eefa6d36a3a5b40
klemensgit <klemen418@gmail.com>
2020-06-21 16:24:56 +0200

    feat(webpack-config):dodelava webpack config fila z moznostjo hesiranja datotek, dinamicnega nalaganja in autoprefixa

--- index.php
+++ index.php
@@ -2,23 +2,11 @@
     require_once './class/DBqueries.php';
     require_once './utils/functions.php';
     require_once './utils/function_custom.php';
+
+    include './vendor/sub_tpl_head.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" type="text/css" href=""/>
-	<link rel="stylesheet" type="text/css" href="" />
-
-    <script type="text/javascript" src=""></script>
-	<script type="text/javascript" src=""></script>
-
-    <title>Template</title>
-</head>
 <body>
     <div class="container">
         <h1>Wellcome to</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
@@ -9,7 +11,7 @@ module.exports = {
         "js/scripts.min":"./js/scripts.js"
     },
     output:{
-        filename: "[name].js",
+        filename: "[name].[contentHash].js",
         path: path.resolve(__dirname, "vendor")
     },
     module: {
@@ -25,14 +27,25 @@ module.exports = {
                         }
                     },
                     'css-loader',
+                    'postcss-loader',
                     'sass-loader'
                 ]
             }
         ]
     },
     plugins:[
+        // extrakta javascript css zapis iz script.js file-a v svoj css file.
         new MiniCssExtractPlugin({
-            filename: "[name].css",
+            filename: "[name].[contentHash].css",
+        }),
+        // počisti odvečne hashfile, ki se generirajo z vsako spremembo.
+        new CleanWebpackPlugin(),
+        // dinamično na podlagi novih hash filov dodaja v head zadnjo verzijo.
+        new HtmlWebpackPlugin({
+            template: './sub_tpl_head.php',
+            inject:'head',
+            filename:'sub_tpl_head.php'
         })
+
     ]
 };
