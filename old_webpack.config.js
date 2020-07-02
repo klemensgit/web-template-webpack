ba1b653148212e73027dc0f4355a6f76124542f9
klemensgit <klemen418@gmail.com>
2020-07-02 13:50:28 +0200

    dodani file-i za test delovanja

---
+++ old_webpack.config.js
@@ -0,0 +1,90 @@
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+var config = {
+    entry: {
+        //"css/style.min":"./sass/style.scss",
+        "vendor.min": "./lib/vendor.js",
+        "scripts.min": "./js/scripts.js"
+    },
+    output:{
+        filename: "js/[name].[contentHash].js",
+        path: path.resolve(__dirname, "vendor"),
+        publicPath: "./vendor/" //pot, da HtmlWebpackPlugin ve kakšno pot nastavit, ko injecta hash file v head
+    },
+    module: {
+        rules:[
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    "targets": {
+                                        "ie": "11"
+                                    }
+                                }
+                            ]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /.(scss|css)$/,
+                exclude: /node_modules/,
+                use:[
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    //'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: (loader) => [
+                                require('autoprefixer')
+                            ]
+                        }
+                    },
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins:[
+        // extrakta javascript css zapis iz script.js file-a v svoj css file.
+        new MiniCssExtractPlugin({
+            filename: "css/style.min.[contentHash].css",
+            //filename: "css/[name].[contentHash].css", če žeilimo da se ohrani ime datoteke iz katere extraktamo css v tem primeru script.js
+        }),
+        // počisti odvečne hashfile, ki se generirajo z vsako spremembo.
+        new CleanWebpackPlugin(),
+        // dinamično na podlagi novih hash filov dodaja v head zadnjo verzijo.
+        new HtmlWebpackPlugin({
+            template: './sub_tpl_head.php',
+            inject:'head',
+            filename:'sub_tpl_head.php'
+        })
+    ]
+};
+
+
+module.exports = (env, argv) => {
+
+    if (argv.mode === 'development') {
+      config.devtool = 'source-map';
+    }
+  
+    if (argv.mode === 'production') {
+      //...
+    }
+  
+    return config;
+};
